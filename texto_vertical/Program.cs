eb2b5df93b11106c39ede540a3636e6667f9985e
Daniel Gomez <dgomez.informatica@gmail.com>
2022-12-02 13:54:41 -0300

    Usuario ingresa frase o palabra, que despues con substring lo separa por caracter

---
+++ texto_vertical/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading;
+
+namespace texto_vertical
+{
+    internal class Program // internal is opcional
+    {
+        static void Main(string[] args)
+        {
+            String frase;
+            int cont = 0; // contador
+
+            Console.WriteLine("Ingrese una frase o palabra");
+            frase = Console.ReadLine();
+
+            Console.WriteLine("Procesando...\n");
+            Thread.Sleep(2000);
+            while (cont < frase.Length)
+            {
+                Console.WriteLine(frase.Substring(cont, 1));
+                cont++;
+            }
+
+
+            Console.WriteLine("\nPresione cualquier tecla para salir...");
+            Console.ReadKey();
+        }
+    }
+}
